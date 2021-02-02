24145eedcd007abcc416ea0941e5b84e6df18798
jerrinot <jaromir.hamala@gmail.com>
2021-02-02 13:04:07 +0100

    allow extracting portable keys

--- jetjob/src/main/java/info/jerrinot/cdcportable/server/job/mapping/ChangeRecordMapper.java
+++ jetjob/src/main/java/info/jerrinot/cdcportable/server/job/mapping/ChangeRecordMapper.java
@@ -27,10 +27,25 @@ public class ChangeRecordMapper {
     public static FunctionEx<? super ChangeRecord, ? extends GenericRecord> valueToPortable(int factoryId,
                                                                                             int classId,
                                                                                             MappingDef mapping) {
-        return new Mapper(factoryId, classId, mapping);
+        return partToPortable(factoryId, classId, mapping, ChangeRecord::value);
     }
 
-    private static class Mapper implements FunctionEx<ChangeRecord, GenericRecord> {
+    public static FunctionEx<? super ChangeRecord, ? extends GenericRecord> keyToPortable(int factoryId,
+                                                                                            int classId,
+                                                                                            MappingDef mapping) {
+        return partToPortable(factoryId, classId, mapping, ChangeRecord::key);
+    }
+
+    private static FunctionEx<ChangeRecord, GenericRecord> partToPortable(int factoryId, int classId, MappingDef mapping,
+                                                                          FunctionEx<ChangeRecord, RecordPart> partExtractor) {
+        var mapper = new Mapper(factoryId, classId, mapping);
+        return (cr) -> {
+            RecordPart value = partExtractor.apply(cr);
+            return mapper.applyEx(value);
+        };
+    }
+
+    private static class Mapper implements FunctionEx<RecordPart, GenericRecord> {
         private transient ClassDefinition cd;
         private String[] froms;
         private String[] tos;
@@ -43,6 +58,64 @@ public class ChangeRecordMapper {
             this.classId = classId;
             expandMappings(mapping);
             initializeClassDefinition();
+        }
+
+        @Override
+        public GenericRecord applyEx(RecordPart value) throws Exception {
+            Map<String, Object> fieldMap = value.toMap();
+            GenericRecord.Builder builder = GenericRecord.Builder.portable(cd);
+            for (int i = 0; i < froms.length; i++) {
+                String from = froms[i];
+                Object o = fieldMap.get(from);
+                if (o == null) {
+                    continue;
+                }
+                FieldType fieldType = types[i];
+                String to = tos[i];
+                switch (fieldType) {
+                    case PORTABLE:
+                        throw new UnsupportedOperationException("nested portables not implemented");
+                    case BYTE:
+                        builder.writeByte(to, (Byte) o);
+                        break;
+                    case BOOLEAN:
+                        builder.writeBoolean(to, (Boolean) o);
+                        break;
+                    case CHAR:
+                        builder.writeChar(to, (Character) o);
+                        break;
+                    case SHORT:
+                        builder.writeShort(to, (Short) o);
+                        break;
+                    case INT:
+                        builder.writeInt(to, (Integer) o);
+                        break;
+                    case LONG:
+                        builder.writeLong(to, (Long) o);
+                        break;
+                    case FLOAT:
+                        builder.writeFloat(to, (Float) o);
+                        break;
+                    case DOUBLE:
+                        builder.writeDouble(to, (Double) o);
+                        break;
+                    case UTF:
+                        builder.writeUTF(to, (String) o);
+                        break;
+                    case PORTABLE_ARRAY:
+                    case BYTE_ARRAY:
+                    case BOOLEAN_ARRAY:
+                    case CHAR_ARRAY:
+                    case SHORT_ARRAY:
+                    case INT_ARRAY:
+                    case LONG_ARRAY:
+                    case FLOAT_ARRAY:
+                    case DOUBLE_ARRAY:
+                    case UTF_ARRAY:
+                        throw new UnsupportedOperationException("nested arrays not implemented");
+                }
+            }
+            return builder.build();
         }
 
         private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
@@ -116,65 +189,6 @@ public class ChangeRecordMapper {
                 }
             }
             cd = cdBuilder.build();
-        }
-
-        @Override
-        public GenericRecord applyEx(ChangeRecord changeRecord) throws Exception {
-            RecordPart value = changeRecord.value();
-            Map<String, Object> fieldMap = value.toMap();
-            GenericRecord.Builder builder = GenericRecord.Builder.portable(cd);
-            for (int i = 0; i < froms.length; i++) {
-                String from = froms[i];
-                Object o = fieldMap.get(from);
-                if (o == null) {
-                    continue;
-                }
-                FieldType fieldType = types[i];
-                String to = tos[i];
-                switch (fieldType) {
-                    case PORTABLE:
-                        throw new UnsupportedOperationException("nested portables not implemented");
-                    case BYTE:
-                        builder.writeByte(to, (Byte) o);
-                        break;
-                    case BOOLEAN:
-                        builder.writeBoolean(to, (Boolean) o);
-                        break;
-                    case CHAR:
-                        builder.writeChar(to, (Character) o);
-                        break;
-                    case SHORT:
-                        builder.writeShort(to, (Short) o);
-                        break;
-                    case INT:
-                        builder.writeInt(to, (Integer) o);
-                        break;
-                    case LONG:
-                        builder.writeLong(to, (Long) o);
-                        break;
-                    case FLOAT:
-                        builder.writeFloat(to, (Float) o);
-                        break;
-                    case DOUBLE:
-                        builder.writeDouble(to, (Double) o);
-                        break;
-                    case UTF:
-                        builder.writeUTF(to, (String) o);
-                        break;
-                    case PORTABLE_ARRAY:
-                    case BYTE_ARRAY:
-                    case BOOLEAN_ARRAY:
-                    case CHAR_ARRAY:
-                    case SHORT_ARRAY:
-                    case INT_ARRAY:
-                    case LONG_ARRAY:
-                    case FLOAT_ARRAY:
-                    case DOUBLE_ARRAY:
-                    case UTF_ARRAY:
-                        throw new UnsupportedOperationException("nested arrays not implemented");
-                }
-            }
-            return builder.build();
         }
     }
 }
